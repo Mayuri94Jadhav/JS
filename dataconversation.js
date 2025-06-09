1c63d2f62d00b6905d709120ec6e1fec70626e1f
mayuri balasaheb jadhav <mayurij528@gmail.com>
2025-06-09 14:11:23 +0530

    dataconversation

---
+++ dataconversation.js
@@ -0,0 +1,23 @@
+// let marks="90abc"
+// console.log(typeof(marks));
+
+// let markss=90
+// console.log(typeof(markss));
+// let value=Number(marks)
+// console.log(value);
+
+// let marksss= null
+//  let value=Number(marksss)
+// console.log(value);
+
+let marksss= undefined
+ let value=Number(marksss)
+console.log(value);
+
+
+
+// "24"=>24
+//"33abc"=>nan
+//tru=> 1;false=> 0;
+// "" => false; "mayuri"=> true
+
