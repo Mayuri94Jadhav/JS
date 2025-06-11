822f6689fb9c58fc8b2160cb158bd99ded99d967
mayuri balasaheb jadhav <mayurij528@gmail.com>
2025-06-11 14:10:35 +0530

    camparison.js

---
+++ camparison.js
@@ -0,0 +1,32 @@
+//Basic conver..
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+
+//change dataType
+
+console.log("2">1);
+console.log("0">1);// avoid to convert
+
+
+console.log(null==0);
+console.log(null>0);
+console.log(null<=0);// it convert null into 0
+
+
+console.log(undefined== 0);
+console.log(undefined> 0);
+console.log(undefined< 0);
+
+// strickly check (===)
+
+console.log("8"==8);
+console.log("8"===8);// ==or=== have diff.
+
+
+
+
+
