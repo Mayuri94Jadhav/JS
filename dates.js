3278343e8aa2c3e1834b551cf7a9895d8b3be9fa
mayuri balasaheb jadhav <mayurij528@gmail.com>
2025-06-23 12:01:18 +0530

    objects.js

---
+++ dates.js
@@ -0,0 +1,43 @@
+let myDate= new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+
+// console.log(typeof myDate);// date is  a object
+
+
+// let newDate=new Date(2025,0,18)
+// console.log(newDate.toDateString());
+
+// let newDate=new Date(2025,0,18,4,7)
+// let newDate=new Date(2025,0,18,4,7)
+// let newDate=new Date("2025-06-18")
+let newDate=new Date("06-18-2025")
+//  console.log(newDate.toLocaleString());
+
+
+let myTimeStamp =Date.now()
+
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());
+// console.log(Math.floor(Date.now()/1000));// convert into seconds
+
+let newmyDate=new Date()
+console.log(newmyDate.getFullYear()+1);
+console.log(` the current month is ${newmyDate.getMonth()}`);
+
+ myDate.toLocalString('default',{
+    weekday:"long"
+ 
+})
+
+
+
+
+
+
+
